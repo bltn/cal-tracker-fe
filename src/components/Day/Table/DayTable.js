27efcb632789702c9a7b4334c227a114746267cd
Ben Lawton <thisisbenlawton@gmail.com>
2020-04-26 11:59:05 +0100

    Only show table content when there are > 0 meal entries

--- src/components/Day/Table/DayTable.js
+++ src/components/Day/Table/DayTable.js
@@ -4,24 +4,27 @@ import CreateMealEntry from "../../MealEntry/Create/CreateMealEntry";
 
 export default class DayTable extends React.Component {
     render() {
+        const noMealEntryComponents = this.props.mealEntryComponents.length === 0;
+        const visibilityToggle = noMealEntryComponents ? "invisible" : "";
+
         return (
             <table>
                 <caption>
                     <span className="dayName">{this.props.name}</span>
                     <DeleteDay key={this.props.id} id={this.props.id} onDayDeleted={this.props.onDayDeleted}/>
                 </caption>
-                <thead>
+                <thead className={visibilityToggle}>
                 <tr>
                     <th>Meal name</th>
                     <th>Calories</th>
                 </tr>
                 </thead>
-                <tbody>
+                <tbody className={visibilityToggle}>
                 {this.props.mealEntryComponents}
                 </tbody>
                 <tfoot>
                 <tr>
-                    <td valign="middle" align="left">Total : {this.props.totalCalories}</td>
+                    <td className={visibilityToggle} valign="middle" align="left">Total : {this.props.totalCalories}</td>
                     <td valign="bottom" align="right">
                         <CreateMealEntry dayId={this.props.id} onMealEntryCreated={this.props.onMealEntryCreated}/>
                     </td>
